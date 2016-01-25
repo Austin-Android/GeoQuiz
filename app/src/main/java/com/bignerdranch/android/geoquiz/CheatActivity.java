d17e5ae4db739937b5d53f472e96b477a2542cea
Darran Kelinske <Darran.Kelinske@servicemesh.com>
2016-01-25 11:08:27 -0600

    back out hungarian notation

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -41,9 +41,9 @@ public class CheatActivity extends AppCompatActivity {
 
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
-        mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+        mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
 
-        mShowAnswer = (Button) findViewById(R.id.showAnswerButton);
+        mShowAnswer = (Button) findViewById(R.id.show_answer_button);
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
